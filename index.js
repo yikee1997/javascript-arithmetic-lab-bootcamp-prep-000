1a333bdc525a425d557bf897a3d00f8f1fe08d86
YI KEE <marioxd1997@gmail.com>
2019-01-31 11:37:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+var number
+
+function add(a, b) {
+  number = a + b ;
+  return number;
+}
+
+function subtract(a, b) {
+  number = a - b;
+  return number;
+}
+
+function multiply(a,b) {
+  number = a * b;
+  return number;
+}
+
+function divide(a, b) {
+  number = a / b;
+  return number;
+}
+
+function inc(n) {
+  number ++;
+  return number;
+}
+
+function dec(n) {
+  number --;
+  return number;
+}
+
+function makeInt(n) {
+  number = parseInt(a, 10);
+  return number;
+}
+
+function preserveDecimal(n) {
+  number = parseFloat(a, 10);
+  return number;
+}
